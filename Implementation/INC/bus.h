e37c8a61ab4f344af9aafb6907e26789bacdb534
krishna <horrific5143@gmail.com>
2021-09-08 23:11:28 +0530

    edited

--- Implementation/INC/bus.h
+++ Implementation/INC/bus.h
@@ -1,9 +1,14 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef __MAIN_FUNCTION_H__
+#define __MAIN_FUNCTION_H__
 
-#include<stdio.h>
-#include<string.h>
-#include "bus.c"
+#include <stdio.h>
 #include <stdlib.h>
+//#include <stdio_ext.h>
 
+int cal(int y1, int y2, int h);
+void details(int k);
+void add_node(char lol[20], char der[6], int b);
+int seat(int p);
+void bill(int y, int j);
+void login();
 #endif
